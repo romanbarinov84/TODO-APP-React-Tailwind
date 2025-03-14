8b824c1f4d2b6b05df21008f367157a9ad92d905
Roman <roman-meinl@ukr.net>
2025-03-14 17:43:43 -0200

    create stylest

--- src/App.jsx
+++ src/App.jsx
@@ -50,17 +50,19 @@ function App() {
 
   return (
     <div data-theme={theme} className='flex flex-col min-h-screen justify-center items-center bg-page-light dark:bg-page-dark p-24'>
-      <div>
-        <div>
-          <button onClick={toggleTheme}>
-            <div>Change Theme</div>
+      <div className='mb-6'>
+        <div className='flex items-center cursor-pointer'>
+          <button className='relative' onClick={toggleTheme}>
+            <div className='w-30 h-6 rounded-full shadow-inner transition-colors duration-300 bg-gray-400 dark:bg-btn-dark'></div>
+            <div className='absolute top-0.5 left-0.5 w-6 h-6 bg-white rounded-full shadow-md transform transition-transform duration-300 translate-x-0 dark:translate-x-20'></div>
           </button>
+          <span className='ml-3 text-grey-700 dark:text-gray-300 font-medium'>{theme === "light" ? "light" : "dark"}</span>
         </div>
       </div>
-      <div>
-        <h1>My todo App</h1>
+      <div className='mx-auto flex flex-col gap-3 '>
+        <h1 className='text-4xl font-bold text-center text-gray-800 dark:text-white mb-8'><span className='bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500'>My todo App</span></h1>
         <AddTodo onAdd={onAdd}/>
-        <div>Component to create tasks</div>
+        <div className='flex flex-col gap-3.5'>Component to create tasks</div>
         <div>
           {todos.map((todo) => (
             <ToDoItem key={todo.id} todo={todo} onDelette={onDelette}/>
--- src/Components/AddTodo.jsx
+++ src/Components/AddTodo.jsx
@@ -12,8 +12,12 @@ export function AddTodo({onAdd}) {
     }
 
     const[text, setText] = useState("")
-    return (<form onSubmit={handleSubmit}>
-        <input type="text" value={text} onChange={(e) => setText(e.target.value) } />
+    return (<form onSubmit={handleSubmit} className="mb-6 ">
+        <div className="flex items-center bg-amber-50 rounded-lg shadow-sky-300 overflow-hidden 
+        border-gray-100 focus-within:ring-2 focus-within:ring-offset-blue-300">
+            <input className="flex-1 p-3 text-gray-700 dark:bg-page-dark dark:text-btn-dark" type="text" value={text} onChange={(e) => setText(e.target.value) } placeholder="Add task..."/>
         <button type="submit">Added</button>
+        </div>
+        
     </form>);
 }

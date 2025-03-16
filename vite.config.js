2438c4444a2aff19867df1ad0aa39ab166fc2337
Roman <roman-meinl@ukr.net>
2025-03-16 16:02:06 -0200

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/TODO-APP-React-Tailwind/", 
+  plugins: [tailwindcss(), react()],
+  base: "/TODO-APP-React-Tailwind/",
 })

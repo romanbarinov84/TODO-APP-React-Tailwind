f08a36695cbad80474d5558a6500bb7e0f7c6361
Roman <roman-meinl@ukr.net>
2025-03-16 15:42:06 -0200

    last commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import tailwindcss from '@tailwindcss/vite'
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/your-repo-name/", // Укажи название репозитория
-  plugins: [react()],
+  plugins: [tailwindcss(),react()],
 })
